a98882bafe870b699233659208b5ab0bc5d498ec
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2022-11-17 20:06:24 -0300

    add initial baseline

---
+++ src/mean_field_location_baseline.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+
+    VAL_YEAR = 2021
+
+    trait = pd.read_csv(
+        'data/Training_Data/1_Training_Trait_Data_2014_2021.csv',
+        usecols=['Year', 'Field_Location', 'Yield_Mg_ha']
+    )
+
+    xtrain = trait[trait['Year'] < VAL_YEAR]
+    yhat = xtrain.groupby('Field_Location')['Yield_Mg_ha'].mean().rename('yhat')
+
+    yval = trait[trait['Year'] == VAL_YEAR][['Field_Location', 'Yield_Mg_ha']]
+    yval = yval.merge(yhat, on='Field_Location', how='left')
+    
+    rmse_per_field = yval.groupby('Field_Location').apply(
+        lambda x: (x['Yield_Mg_ha'] - x['yhat']).pow(2).mean() ** 0.5
+    )
+    rmse = sum(rmse_per_field) / len(rmse_per_field)
+    
+    print(rmse_per_field.describe())
+    print('RMSE:', rmse)
+    print(yval.corr())
+
+
+
+
