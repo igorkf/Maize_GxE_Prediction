a7cb74e27b44cd136efa7265ea57ebc178448ec0
igorkf <igorkuivjogi@hotmail.com>
2023-02-15 16:10:54 -0600

    fix gxe sheel script

--- run_gxe_models.sh
+++ run_gxe_models.sh
@@ -4,5 +4,5 @@ echo 'A model ok'
 python3 src/run_g_or_gxe_model.py --model=GxE --D --svd --n_components=150 > logs/gxe_model_D_svd.txt
 echo 'D model ok'
 
-# python3 src/run_g_or_gxe_model.py --model=GxE --epiAA --epiDD --epiAD --svd > logs/g_model_epiAA_epiDD_epiAD_svd.txt
-# echo 'epiAA epiDD epiAD model ok'
+python3 src/run_g_or_gxe_model.py --model=GxE --epiAA --epiDD --epiAD --svd > logs/gxe_model_epiAA_epiDD_epiAD_svd.txt
+echo 'epiAA epiDD epiAD model ok'
