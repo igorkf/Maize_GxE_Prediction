1b7bc6f5fd8ff1decf05b7381ef0320b638ddc09
igorkf <igorkuivjogi@hotmail.com>
2023-02-15 14:16:29 -0600

    add e model scripts

--- src/run_e_model.py
+++ src/run_e_model.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import lightgbm as lgbm
-from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
 from preprocessing import process_test_data
+from evaluate import create_df_eval, avg_rmse
+
 
 TRAIT_PATH = 'data/Training_Data/1_Training_Trait_Data_2014_2021.csv'
 TEST_PATH = 'data/Testing_Data/1_Submission_Template_2022.csv'
@@ -11,7 +13,6 @@ META_TEST_PATH = 'data/Testing_Data/2_Testing_Meta_Data_2022.csv'
 
 
 if __name__ == '__main__':
-
     df_sub = process_test_data(TEST_PATH).reset_index()[['Env', 'Hybrid']]
 
     xtrain = pd.read_csv('output/xtrain.csv')
@@ -38,36 +39,25 @@ if __name__ == '__main__':
     # print(feat_imp)
 
     # predict
-    yhat = model.predict(xval)
-    df_eval = pd.DataFrame({
-        'Field_Location': yval.index.get_level_values(0).str.replace('(_).*', '', regex=True),
-        'Env': yval.index.get_level_values(0),
-        # 'Hybrid': yval.index.get_level_values(1),
-        'ytrue': yval,
-        'yhat': yhat
-    })
-    df_eval.to_csv('output/oof_e_model.csv', index=False)
+    ypred = model.predict(xval)
+
+    # evaluate
+    df_eval = create_df_eval(xval, yval, ypred)
+    _ = avg_rmse(df_eval)
+
+    # write
+    outfile = 'output/oof_e_model.csv'
+    print('Writing:', outfile)
+    df_eval.to_csv(outfile, index=False)
 
     # predict on test
-    df_sub['Yield_Mg_ha'] = model.predict(xtest)
-    df_sub.to_csv('output/submission.csv', index=False)
+    # df_sub['Yield_Mg_ha'] = model.predict(xtest)
+    # df_sub.to_csv('output/submission.csv', index=False)
     
-    # evaluate
-    rmse_per_field = df_eval.groupby('Field_Location').apply(
-        lambda x: (x['ytrue'] - x['yhat']).pow(2).mean() ** 0.5
-    )
-    rmse = sum(rmse_per_field) / len(rmse_per_field)
-    print(rmse_per_field)
-    print('RMSE (per location):\n', rmse_per_field.describe().to_frame('   ').T)
-    print('RMSE:', rmse)
-    r2 = r2_score(df_eval['ytrue'], df_eval['yhat'])
-    adj_r2 = 1 - (1 - r2) * (len(df_eval) - 1) / (len(df_eval) - df_eval.shape[1] - 1)
-    print('Adj. R2:', adj_r2)
-
     # observed X predicted statistics
-    obs_vs_pred = pd.concat([
-        df_eval['ytrue'].rename('observed').describe(),
-        df_eval['yhat'].rename('predicted').describe(),
-        df_sub['Yield_Mg_ha'].rename('submission').describe()
-    ], axis=1)
-    print(obs_vs_pred)
+    # obs_vs_pred = pd.concat([
+    #     df_eval['ytrue'].rename('observed').describe(),
+    #     df_eval['yhat'].rename('predicted').describe(),
+    #     df_sub['Yield_Mg_ha'].rename('submission').describe()
+    # ], axis=1)
+    # print(obs_vs_pred)
