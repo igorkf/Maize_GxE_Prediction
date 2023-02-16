4658264c3710951e3f6f603679b009765c67c257
igorkf <igorkuivjogi@hotmail.com>
2023-02-16 17:06:11 -0600

    disable buffer output

--- run_g_models.sh
+++ run_g_models.sh
@@ -1,8 +1,8 @@
-python3 src/run_g_or_gxe_model.py --model=G --A --svd > logs/g_model_A_svd.txt &&
+python3 -u src/run_g_or_gxe_model.py --model=G --A --svd > logs/g_model_A_svd.txt &&
 echo 'A model ok' &&
 
-python3 src/run_g_or_gxe_model.py --model=G --D --svd > logs/g_model_D_svd.txt &&
+python3 -u src/run_g_or_gxe_model.py --model=G --D --svd > logs/g_model_D_svd.txt &&
 echo 'D model ok' &&
 
-python3 src/run_g_or_gxe_model.py --model=G --epiAA --epiDD --epiAD --svd > logs/g_model_epiAA_epiDD_epiAD_svd.txt &&
+python3 -u src/run_g_or_gxe_model.py --model=G --epiAA --epiDD --epiAD --svd > logs/g_model_epiAA_epiDD_epiAD_svd.txt &&
 echo 'epiAA epiDD epiAD model ok'
