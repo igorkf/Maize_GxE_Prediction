766aea37388b46984f4e7723952bc7575884e7d3
igorkf <igorkuivjogi@hotmail.com>
2023-02-28 11:49:30 -0600

    add cv argument in all shell scripts

--- run_gxe_models.sh
+++ run_gxe_models.sh
@@ -1,10 +1,10 @@
 cv=$1
 
-python3 -u src/run_g_or_gxe_model.py --model=GxE --A --svd > "logs/gxe_model_A_svd_cv${cv}.txt" &&
+python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=GxE --A --svd > "logs/gxe_model_A_svd_cv${cv}.txt" &&
 echo '[GxE] A model ok' &&
 
-python3 -u src/run_g_or_gxe_model.py --model=GxE --D --svd --n_components=150 > "logs/gxe_model_D_svd_cv${cv}.txt" &&
+python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=GxE --D --svd --n_components=150 > "logs/gxe_model_D_svd_cv${cv}.txt" &&
 echo '[GxE] D model ok' &&
 
-python3 src/run_g_or_gxe_model.py --model=GxE --epiAA --epiDD --epiAD --svd > "logs/gxe_model_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
+python3 src/run_g_or_gxe_model.py --cv=${cv} --model=GxE --epiAA --epiDD --epiAD --svd > "logs/gxe_model_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
 echo '[GxE] epiAA epiDD epiAD model ok'
