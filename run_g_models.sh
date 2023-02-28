2ad222b05d791b49d7fcbb5ef2a77f162a525ee0
igorkf <igorkuivjogi@hotmail.com>
2023-02-27 20:20:49 -0600

    add logs for g models

--- run_g_models.sh
+++ run_g_models.sh
@@ -6,3 +6,6 @@ echo 'D model ok' &&
 
 python3 -u src/run_g_or_gxe_model.py --model=G --epiAA --epiDD --epiAD --svd > logs/g_model_epiAA_epiDD_epiAD_svd.txt &&
 echo 'epiAA epiDD epiAD model ok'
+
+python3 -u src/run_g_or_gxe_model.py --model=G --A --D --epiAA --epiDD --epiAD --svd > logs/g_model_A_D_epiAA_epiDD_epiAD_svd.txt &&
+echo 'A D epiAA epiDD epiAD model ok'
