dcbd88ad4062d04b00bb3d4178c0dddf12633e41
igorkf <igorkuivjogi@hotmail.com>
2023-05-15 22:18:42 -0500

    fix kronecker calculations

--- src/kronecker.R
+++ src/kronecker.R
@@ -1,10 +1,17 @@
 library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
-cv <- args[1]  # 0, 1, or 2
-debug <- as.logical(args[2])  # FALSE or TRUE  (for debugging set to TRUE)
-dataset <- args[3]  # "train" or "val"
-kinship_type <- args[4]  # "additive", "dominant", "epi_AA", "epi_DD", or "epi_AD"
+if (length(args) == 0) {
+  cv <- 0
+  debug <- FALSE
+  dataset <- "train"
+  kinship_type <- "additive"
+} else {
+  cv <- args[1]  # 0, 1, or 2
+  debug <- as.logical(args[2])  # FALSE or TRUE  (for debugging set to TRUE)
+  dataset <- args[3]  # "train" or "val"
+  kinship_type <- args[4]  # "additive", "dominant", "epi_AA", "epi_DD", or "epi_AD"
+}
 kinship_path <- paste0("output/kinship_", kinship_type, ".txt")
 outfile <- paste0("output/cv", cv, "/kronecker_", kinship_type, "_", dataset, ".feather")
 cat("Debug mode:", debug, "\n")
@@ -31,18 +38,19 @@ if (debug == FALSE) {
 }
 colnames(kinship) <- substr(colnames(kinship), 1, nchar(colnames(kinship)) / 2)  # fix column names
 kinship <- as.matrix(kinship)
-rownames(kinship) <- colnames(kinship)[1:dim(kinship)[1]]
-kinship <- kinship[rownames(kinship) %in% y$Hybrid, ]
+rownames(kinship) <- colnames(kinship)
+kinship <- kinship[rownames(kinship) %in% y$Hybrid, colnames(kinship) %in% y$Hybrid]
 
 K <- kronecker(x, kinship, make.dimnames = T)
 rm(x); rm(kinship); gc()
 cat("K dim:", dim(K), "\n")
 
+# some Env:Hybrid combinations does not exist so we can remove
 K <- K[rownames(K) %in% paste0(y$Env, ":", y$Hybrid), ]
 rm(y)
 cat("K dim:", dim(K), "\n")
 
-# write to feather for fast reading (~3.5 min)
+# write to feather for fast reading
 arrow::write_feather(
   data.frame(id = rownames(K), K), 
   outfile,
