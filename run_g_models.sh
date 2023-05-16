80785a2131316db2f1460297360a799304ef7272
igorkf <igorkuivjogi@hotmail.com>
2023-05-15 22:17:53 -0500

    add more svd components to epistatic

--- run_g_models.sh
+++ run_g_models.sh
@@ -6,7 +6,7 @@ echo '[G] A model ok' &&
 python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --D --svd > "logs/g_model_D_svd_cv${cv}.txt" &&
 echo '[G] D model ok' &&
 
-python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --epiAA --epiDD --epiAD --svd > "logs/g_model_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
+python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --epiAA --epiDD --epiAD --svd --n_components=250 > "logs/g_model_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
 echo '[G] epiAA epiDD epiAD model ok' &&
 
 python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --A --D --epiAA --epiDD --epiAD --svd > "logs/g_model_A_D_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
