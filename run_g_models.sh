c30ae8b62620960da0a87446ba2f9b05e25bcbf9
igorkf <igorkuivjogi@hotmail.com>
2023-05-31 13:28:30 -0500

    fix number of components for g models

--- run_g_models.sh
+++ run_g_models.sh
@@ -12,7 +12,7 @@ python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --epiAA --epiDD --epiA
 python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --epiAA --epiDD --epiAD --svd --n_components=250 --lag_features > "logs/g_model_epiAA_epiDD_epiAD_svd_cv${cv}_lag_features.txt" &&
 echo '[G] epiAA epiDD epiAD model ok' &&
 
-python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --A --D --epiAA --epiDD --epiAD --svd > "logs/g_model_A_D_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
-python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --A --D --epiAA --epiDD --epiAD --svd --lag_features > "logs/g_model_A_D_epiAA_epiDD_epiAD_svd_cv${cv}_lag_features.txt" &&
+python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --A --D --epiAA --epiDD --epiAD --svd --n_components=250 > "logs/g_model_A_D_epiAA_epiDD_epiAD_svd_cv${cv}.txt" &&
+python3 -u src/run_g_or_gxe_model.py --cv=${cv} --model=G --A --D --epiAA --epiDD --epiAD --svd --n_components=250 --lag_features > "logs/g_model_A_D_epiAA_epiDD_epiAD_svd_cv${cv}_lag_features.txt" &&
 echo '[G] A D epiAA epiDD epiAD model ok'
 
