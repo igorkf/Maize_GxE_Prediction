6603b05d599a20fa58fcd7a3b3aec5be835ba42e
igorkf <igorkuivjogi@hotmail.com>
2023-07-26 14:19:12 -0500

    fix kronecker calculations

--- run_kroneckers.sh
+++ run_kroneckers.sh
@@ -4,9 +4,6 @@ debug=FALSE
 
 for kinship in additive dominant epi_AA epi_DD epi_AD
 do 
-    for dataset in train val 
-    do
-        Rscript src/kronecker.R $cv $fold $debug $dataset $kinship > "logs/kronecker_${kinship}_${dataset}_cv${cv}_fold${fold}.txt" &&
-        echo "[Kronecker] ${kinship} for ${dataset} ok"
-    done
+    Rscript src/kronecker.R $cv $fold $debug $kinship > "logs/kronecker_${kinship}_cv${cv}_fold${fold}.txt" &&
+    echo "[Kronecker] ${kinship}"
 done
--- src/kronecker.R
+++ src/kronecker.R
@@ -4,34 +4,33 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
   cv <- 0
   fold <- 0
-  debug <- FALSE
-  dataset <- "train"
+  debug <- TRUE
   kinship_type <- "additive"
 } else {
   cv <- args[1]  # 0, 1, or 2
   fold <- args[2]  # 0, 1, 2, 3 or 4
-  debug <- as.logical(args[3])  # FALSE or TRUE  (for debugging set to TRUE)
-  dataset <- args[4]  # "train" or "val"
-  kinship_type <- args[5]  # "additive", "dominant", "epi_AA", "epi_DD", or "epi_AD"
+  debug <- as.logical(args[3])  # FALSE or TRUE (for debugging set to TRUE)
+  kinship_type <- args[4]  # "additive", "dominant", "epi_AA", "epi_DD", or "epi_AD"
 }
 kinship_path <- paste0("output/kinship_", kinship_type, ".txt")
-outfile <- paste0("output/cv", cv, "/kronecker_", kinship_type, "_", dataset, "_fold", fold, ".feather")
+outfile <- paste0("output/cv", cv, "/kronecker_", kinship_type, "_fold", fold, ".feather")
 cat("Debug mode:", debug, "\n")
 cat("Using", kinship_type, "matrix\n")
-cat("dataset:", dataset, "\n")
 
-x <- fread(paste0("output/cv", cv, "/x", dataset, "_fold", fold, ".csv"), data.table = F)
+xtrain <- fread(paste0("output/cv", cv, "/xtrain_fold", fold, ".csv"), data.table = F)
+xval <- fread(paste0("output/cv", cv, "/xval_fold", fold, ".csv"), data.table = F)
+x <- rbind(xtrain, xval)
 x <- x[, !grepl("yield_lag", colnames(x))]  # remove all yield related features
-x$Hybrid <- NULL 
-x$Env <- substr(x$Env, 1, nchar(x$Env) - 5)
+x$Hybrid <- NULL
 x <- aggregate(x, by = list(x$Env), FUN = tail, n = 1)  # keep only last row of each Env
 x$Group.1 <- NULL
 rownames(x) <- x$Env
 x$Env <- NULL
 x <- as.matrix(x)
 
-y <- fread(paste0("output/cv", cv, "/y", dataset, "_fold", fold, ".csv"), data.table = F)
-y$Env <- substr(y$Env, 1, nchar(y$Env) - 5)
+ytrain <- fread(paste0("output/cv", cv, "/ytrain_fold", fold, ".csv"), data.table = F)
+yval <- fread(paste0("output/cv", cv, "/yval_fold", fold, ".csv"), data.table = F)
+y <- rbind(ytrain, yval)
 
 if (debug == FALSE) {
   kinship <- fread(kinship_path, data.table = F)
@@ -47,7 +46,7 @@ K <- kronecker(x, kinship, make.dimnames = T)
 rm(x); rm(kinship); gc()
 cat("K dim:", dim(K), "\n")
 
-# some Env:Hybrid combinations does not exist so we can remove
+# some Env:Hybrid combinations were not phenotyped
 K <- K[rownames(K) %in% paste0(y$Env, ":", y$Hybrid), ]
 rm(y)
 cat("K dim:", dim(K), "\n")
